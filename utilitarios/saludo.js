dac11d4c4aad25d843bfaaa38a04b339f8d7fc73
Jovir69 <jovirodriguezgye@gmail.com>
2024-01-24 17:52:06 -0500

    Función recuperarTexto Ok

---
+++ utilitarios/saludo.js
@@ -0,0 +1,12 @@
+saludar = function(){
+    //recuperar el valor de la caja de texto
+    let nombre=recuperarTexto("txtNombre");
+    let apellido=recuperarTexto("txtApellido");
+}
+recuperarTexto = function(idComponente){
+    let componente;
+    let valorIngresado;
+    componente = document.getElementById(idComponente);
+    valorIngresado = componente.value;
+    return valorIngresado;
+}
