1c8ab0b9a7358b72395656ce6a51e2b817b86255
Jovir69 <jovirodriguezgye@gmail.com>
2024-01-25 23:02:39 -0500

    Cuarto caso de prueba - Mostrar Valor Total

--- puntoDeVenta_v4/puntoDeVenta.js
+++ puntoDeVenta_v4/puntoDeVenta.js
@@ -38,7 +38,7 @@ calcularValorTotal = function(){
             Si el caso de prueba es exitoso, hacer un commit
          */
     //8. Invocar a calcularIVA y lo que devuelve guardar en la variable valorIVA
-    valorIVA = calcularIVA((valorSubtotal-valorDescuento));
+    valorIVA = calcularIVA(valorSubtotal - valorDescuento);
 	//   El IVA debe calcularse sobre el valor del subtotal menos el descuento
     //9. Mostrar el resultado en el componente lblValorIVA
     mostrarTexto("lblValorIVA",valorIVA);
@@ -52,7 +52,9 @@ calcularValorTotal = function(){
             Si el caso de prueba es exitoso, hacer un commit
         */
     //10. Invocar a calcularTotal y lo que devuelve guardar en la variable valorTotal
+    valorTotal = calcularTotal(valorSubtotal,valorDescuento,valorIVA)
     //11. Mostrar el resultado en el componente lblTotal
+    mostrarTexto("lblTotal",valorTotal);
      /*
             Caso de prueba: 
                 precioProducto: 5.4  cantidad: 10 descuento: 10
