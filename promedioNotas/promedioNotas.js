d4fe5c1c9f376ea06cae72a6b3faa87eef7b0b14
JovirEc <jovirodriguezgye@gmail.com>
2024-02-04 11:09:48 -0500

    Se agregaron las condiciones para reprobado, buen trabajo, excelente y datos incorrectos

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -5,10 +5,18 @@ calcularPromedioNotas = function(){
     let cmpPromedio = calcularPromedio(nota1,nota2,nota3);
     promedio = "Su promedio es: "+cmpPromedio.toFixed(2);
     mostrarTexto("lblPromedio",promedio);
-    if(cmpPromedio>7){
-        mostrarImagen("lblImagen","./imagenes/exito.gif");
+    if(cmpPromedio > 0 && cmpPromedio < 5){
+        mostrarImagen("lblImagen","./imagenes/reprobado.gif");
+        mostrarTexto("lblMensaje","REPROBADO");
+    }else if(cmpPromedio >= 5 && cmpPromedio <= 8){
+        mostrarImagen("lblImagen","./imagenes/bienHecho.gif");
+        mostrarTexto("lblMensaje","BUEN TRABAJO");
+    }else if(cmpPromedio > 8 && cmpPromedio <= 10){
+        mostrarImagen("lblImagen","./imagenes/excelente.gif");
+        mostrarTexto("lblMensaje","EXCELENTE");
     }else{
-        mostrarImagen("lblImagen","./imagenes/fracaso.gif");
+        mostrarImagen("lblImagen","./imagenes/incorrecto.gif");
+        mostrarTexto("lblMensaje","DATOS INCORRECTOS");
     }
 }
 
