d8af3ca7bf087626022bf0c985d6397dc70b9c59
Jovir69 <jovirodriguezgye@gmail.com>
2024-02-07 12:01:23 -0500

    Funcionalidad 100%

--- puntoDeVentaV2/puntoDeVenta.js
+++ puntoDeVentaV2/puntoDeVenta.js
@@ -1,54 +1,89 @@
 calcularValorTotal = function(){
     let nombreProducto;
-    let precioProducto = 0;
-    let cantidad = 0;
+    let precioProducto;
+    let cantidad;
     let valorSubtotal;
     let valorDescuento;
     let valorIVA;
     let valorTotal;
     let resumen;
 
-    nombreProducto = recuperarTexto("txtProducto");
-    if(nombreProducto.length == 0){
-        mostrarTexto("lblErrorNombre","*Campo obligatorio");
+    nombreProducto = recuperarTexto ("txtProducto");
+    cantidad       = recuperarInt   ("txtCantidad");
+    precioProducto = recuperarFloat ("txtPrecio");
+
+    if(esProductoValido (nombreProducto,"lblErrorProducto") & esCantidadValida (cantidad,      "lblErrorCantidad") & esPrecioValido   (precioProducto,"lblErrorPrecio")){
+            valorSubtotal = calcularSubtotal(precioProducto,cantidad);
+            mostrarTexto("lblSubtotal",valorSubtotal);
+
+            valorDescuento = calcularDescuentoPorVolumen(valorSubtotal,cantidad);
+            mostrarTexto("lblDescuento",valorDescuento);
+
+            valorIVA = calcularIVA(valorSubtotal - valorDescuento);
+            mostrarTexto("lblValorIVA",valorIVA);
+
+            valorTotal = calcularTotal(valorSubtotal,valorDescuento,valorIVA)
+            mostrarTexto("lblTotal",valorTotal);
+
+            resumen = mostrarResumen(nombreProducto,cantidad,valorDescuento,valorTotal);
+            mostrarTexto("lblResumen",resumen);
     }
-    cantidad = recuperarInt("txtCantidad");
-    if(cantidad == 0 ){
-        mostrarTexto("lblErrorCantidad","*Campo obligatorio")
+}
+
+esProductoValido = function(producto,idComponenteError){
+    let hayError = false;
+    if(producto.length == 0){
+        mostrarTexto(idComponenteError,"*Campo Obligatorio")
+        hayError = true;
     }
-    precioProducto = recuperarFloat("txtPrecio");
-    if(precioProducto.length == 0){
-        mostrarTexto("lblErrorCantidad","*Campo obligatorio");
+    if(producto.length < 0 || producto.length > 10){
+        mostrarTexto(idComponenteError,"*Cantidad debe ser entre 1 y 10");
+        hayError = true;
     }
-    
-
-    valorSubtotal = calcularSubtotal(precioProducto,cantidad);
-    mostrarTexto("lblSubtotal",valorSubtotal);
-    valorDescuento = calcularDescuentoPorVolumen(valorSubtotal,cantidad);
-    mostrarTexto("lblDescuento",valorDescuento);
-    valorIVA = calcularIVA(valorSubtotal - valorDescuento);
-    mostrarTexto("lblValorIVA",valorIVA);
-    valorTotal = calcularTotal(valorSubtotal,valorDescuento,valorIVA)
-    mostrarTexto("lblTotal",valorTotal);
-    resumen = mostrarResumen(nombreProducto,cantidad,valorDescuento,valorTotal);
-    mostrarTexto("lblResumen",resumen);    
+    if(hayError == false){
+        mostrarTexto(idComponenteError,"");
+    }
+    return !hayError
 }
 
-esProductoValido = function(){
+esCantidadValida = function(cantidad,idComponenteError){
+    let hayError = false;
 
+    if(isNaN(cantidad)){
+        mostrarTexto(idComponenteError,"*Campo Obligatorio")
+        hayError = true;
+    }
+    if(cantidad < 0 || cantidad > 100){
+        mostrarTexto(idComponenteError,"Cantidad debe ser entre 1 y 100")
+        hayError = true;
+    }
+    if(hayError == false){
+        mostrarTexto(idComponenteError,"");
+    }
+    return !hayError
 }
-esCantidadValida = function(){
 
+esPrecioValido = function(precio,idComponenteError){
+    let hayError = false;
+    if(isNaN(precio)){
+        mostrarTexto(idComponenteError,"*Campo Obligatorio");
+        hayError = true;
+    }
+    if(precio < 0 || precio > 50){
+        mostrarTexto(idComponenteError,"Precio debe ser entre 0.01 y 50");
+        hayError = true;
+    }
+    if(hayError == false){
+        mostrarTexto(idComponenteError,"");
+    }
+    return !hayError;
 }
-esPrecioValido = function(){
 
-}
 
 limpiar=function(){
     mostrarTextoEnCaja("txtProducto","");
     mostrarTextoEnCaja("txtCantidad",0);
     mostrarTextoEnCaja("txtPrecio",0.0);
-    mostrarTextoEnCaja("txtPorcentajeDescuento",0)
     mostrarTexto("lblSubtotal",0.0);
     mostrarTexto("lblDescuento",0.0);
     mostrarTexto("lblValorIVA",0.0);
