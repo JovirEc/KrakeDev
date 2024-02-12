9024c1cc9c36b4d821ae9a252d68828dcddd4ccd
Jovir69 <jovirodriguezgye@gmail.com>
2024-02-11 22:45:48 -0500

    Se agregaron las puntuaciones y validaciones de que se detenga al puntaje 5

--- juego/juego.js
+++ juego/juego.js
@@ -7,17 +7,26 @@ jugar = function(seleccionado){
     let rutaJg = generarRuta(seleccionado);
     let resultado = determinarGanador(seleccionado,computadora);
 
-    if(resultado == 0){
-        mostrarTexto("lblResultado1","EMPATE");
-    }else if(resultado == 1){
-        mostrarTexto("lblResultado1","GANASTE LA PARTIDA");
-        puntosUsuario = puntosUsuario + 1;
-    }else{
-        mostrarTexto("lblResultado1","PERDISTE LA PARTIDA");
-        puntosComputador = puntosComputador + 1;
+    if(puntosUsuario < 5 && puntosComputador < 5){
+        if(resultado == 0){
+            mostrarTexto("lblResultado1","EMPATE");
+        }else if(resultado == 1){
+            mostrarTexto("lblResultado1","GANASTE LA PARTIDA");
+            puntosUsuario = puntosUsuario + 1;
+        }else{
+            mostrarTexto("lblResultado1","PERDISTE LA PARTIDA");
+            puntosComputador = puntosComputador + 1;
+        }
+        mostrarImagen("lblImgPc",rutaPc);
+        mostrarImagen("lblImgJugador",rutaJg);
+        mostrarTexto("puntajeJugador",puntosUsuario);
+        mostrarTexto("puntajeComputador",puntosComputador);
     }
-    mostrarImagen("lblImgPc",rutaPc);
-    mostrarImagen("lblImgJugador",rutaJg);
-    mostrarTexto("puntajeJugador",puntosUsuario);
-    mostrarTexto("puntajeComputador",puntosComputador);
+    if(puntosUsuario < 5 && puntosComputador < 5){
+    }else if(puntosUsuario == 5){
+        mostrarTexto("lblResultado1","¡¡ HAS GANADO EL JUEGO !!");
+    
+    }else if(puntosComputador == 5){
+        mostrarTexto("lblResultado1","¡¡ HAS PERDIDO EL JUEGO !!");
+    }
 }
