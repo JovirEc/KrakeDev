b957171cb05ec9459da78e5f727e91b5f7610c94
Jovir69 <jovirodriguezgye@gmail.com>
2024-02-11 22:11:39 -0500

    Se agregó la puntuación y la visualización de la misma

--- juego/juego.js
+++ juego/juego.js
@@ -1,3 +1,6 @@
+let puntosUsuario = 0;
+let puntosComputador = 0;
+
 jugar = function(seleccionado){
     let computadora = generarElemento();
     let rutaPc = generarRuta(computadora);
@@ -8,9 +11,13 @@ jugar = function(seleccionado){
         mostrarTexto("lblResultado1","EMPATE");
     }else if(resultado == 1){
         mostrarTexto("lblResultado1","GANASTE LA PARTIDA");
+        puntosUsuario = puntosUsuario + 1;
     }else{
         mostrarTexto("lblResultado1","PERDISTE LA PARTIDA");
+        puntosComputador = puntosComputador + 1;
     }
     mostrarImagen("lblImgPc",rutaPc);
     mostrarImagen("lblImgJugador",rutaJg);
+    mostrarTexto("puntajeJugador",puntosUsuario);
+    mostrarTexto("puntajeComputador",puntosComputador);
 }
