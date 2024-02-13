7cc5532512eca1913c5a9f7ef9bea7283fe30400
Jovir69 <jovirodriguezgye@gmail.com>
2024-02-13 13:51:33 -0500

    Paso 1 completado

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,4 +1,7 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+let palabraSecreta;
+
+
 esMayuscula = function(caracter){
     let var1 = caracter.charCodeAt(0);
     if(var1 >= 65 && var1 <= 90){
@@ -6,4 +9,28 @@ esMayuscula = function(caracter){
     }else{
         return false;
     }
+}
+
+guardarPalabra = function (){
+
+    let hayError = false;
+    let palabra = recuperarTexto("txtSecreta");
+    let palabralength = palabra.length;
+    //Proceso verificar
+    if (palabralength != 5){
+        hayError = true;
+    }
+    for (let i = 0; i <= 4; i++){
+        if(palabra.charCodeAt(i) < 65 || palabra.charCodeAt(i) > 90){
+            hayError = true;
+        }
+    }
+    //Validación
+    if (hayError == true){
+        alert("*DEBE INGRESAR UNA PLABRA DE 5 LETRAS MAYÚSCULAS*");
+    }else{
+        palabraSecreta = palabra;
+        console.log(palabraSecreta);
+    }
+
 }
