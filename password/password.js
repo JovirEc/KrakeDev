517e48b2775f5e6e851aae10411c36d438c26e39
Jovir69 <jovirodriguezgye@gmail.com>
2024-02-13 12:25:46 -0500

    Pequeñas correcciones al ejercicio de password

--- password/password.js
+++ password/password.js
@@ -10,70 +10,94 @@ ejecutarValidacion = function(){
  
 validarPassword = function(password){//"ABC1234"
     let msjError = "";
-    let validacionError = false;
+    let passlength = password.length;
+    let hayError = false;
     let cumpleCondicion = false;
 
     //ENTRE 8 Y 16 CARACTERES
-    if(password.length < 8 || password.length > 16){
-        validacionError = true;
+    if(passlength < 8 || passlength > 16){
+        hayError = true;
         msjError += "*Debe tener entre 8 y 16 caracteres ";   
     }
     // LETRA MAYUSCULA
-    for(let i = 0; i <= password.length-1; i++){
-        let caracter = password.charAt(i);
-        if(caracter.charCodeAt(i) >= 65 && caracter.charCodeAt(i) <= 90){
+    for(let i = 0; i <= passlength-1; i++){
+        if(password.charCodeAt(i) >= 65 && password.charCodeAt(i) <= 90){
             cumpleCondicion = true;
         }
-    }
+        }
     if(cumpleCondicion == false){
-        validacionError = true;
         msjError += "*Minimo una letra mayuscula "
+        hayError = true;
     }else{
         cumpleCondicion = false;
     }
 
     // NUMEROS
-    for(let i = 0; i <= password.length-1; i++){
+    for(let i = 0; i <= passlength-1; i++){
+        if(password.charCodeAt(i) >= 48 && password.charCodeAt(i) <= 57){
+            cumpleCondicion = true;
+        }
+        }
+    if(cumpleCondicion == false){
+        msjError += "*Minimo un numero "
+        hayError = true;
+    }else{
+        cumpleCondicion = false;
+    }
+    /*for(let i = 0; i <= password.length-1; i++){
         let caracter = password.charAt(i);
         if(caracter >= 0 && caracter <= 9){
             cumpleCondicion = true;
         }
     }
-    /*for(let i = 0; i <= password.length-1; i++){
+    for(let i = 0; i <= password.length-1; i++){
         let caracter = password.charAt(i);
         if(caracter.charCodeAt(i) >= 48 && caracter.charCodeAt(i) <= 57){
             cumpleCondicion = true;
         }
-    }*/
+    }
     if(cumpleCondicion == false){
-        validacionError = true;
+        hayError = true;
         msjError += "*Minimo un numero "
     }else{
         cumpleCondicion = false;
-    }
+    }*/
 
     // CARACTER ESPECIAL
+
+    for(let i = 0; i <= passlength-1; i++){
+        if(password.charCodeAt(i) == 42 || password.charCodeAt(i) == 45 || password.charCodeAt(i) == 95){
+            cumpleCondicion = true;
+        }
+        }
+    if(cumpleCondicion == false){
+        msjError += "*Minimo un caracter especial "
+        hayError = true;
+    }else{
+        cumpleCondicion = false;
+    }
+    {/*
     for(let i = 0; i <= password.length-1; i++){
         let caracter = password.charAt(i);
         if(caracter == "*" || caracter == "-" || caracter == "_"){
             cumpleCondicion = true;
         }
     }
-    /*for(let i = 0; i <= password.length-1; i++){
+    for(let i = 0; i <= password.length-1; i++){
         let caracter = password.charAt(i);
         if(caracter.charCodeAt(i) == 42 || caracter.charCodeAt(i) == 45 || caracter.charCodeAt(i) == 95){
             cumpleCondicion = true;
         }
-    }*/
+    }
     if(cumpleCondicion == false){
-        validacionError = true;
+        hayError = true;
         msjError += "*Minimo un caracter especial "
     }else{
         cumpleCondicion = false;
+    }*/
     }
-    
     // COMPROBAR ERRORES
-    if(validacionError == true){
+    if(hayError == true){
         return msjError;
     }else{
         return "";
--- test/test.js
+++ test/test.js
@@ -1,3 +1,39 @@
-probar = function(){
-    alert("Hola"+""+""+""+""+""+" 2")
+
+validarPassword = function(){
+    let password = recuperarTexto("lblPassword");
+    let passlength = password.length - 1 ;
+    let cumpleCondicion = false;
+    let msgError = "";
+
+
+    for(let i = 0; i <= passlength ; i++){
+
+        if(password.charCodeAt(i) >= 48 && password.charCodeAt(i) <= 57){
+            cumpleCondicion = true;
+        }
+
+        if(cumpleCondicion == true){
+            mostrarTexto("lblResultado","Si hay numero")
+        }else{
+            mostrarTexto("lblResultado","No hay numero")
+        }
+    }
+}
+
+
+
+
+
+recuperarTexto = function(idComponente){
+    let componente;
+    let valorIngresado;
+    componente = document.getElementById(idComponente)
+    valorIngresado = componente.value;
+    return valorIngresado;
+
+}
+mostrarTexto = function(idComponente,mensaje){
+    let componente;
+    componente = document.getElementById(idComponente);
+    componente.innerText = mensaje;
 }
