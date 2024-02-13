c3a0773989f9201c9864cd8dde48e3e50bdee3de
Jovir69 <jovirodriguezgye@gmail.com>
2024-02-13 13:04:55 -0500

    Ahorcado Paso 0 completado

---
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -0,0 +1,9 @@
+//No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+esMayuscula = function(caracter){
+    let var1 = caracter.charCodeAt(0);
+    if(var1 >= 65 && var1 <= 90){
+        return true;
+    }else{
+        return false;
+    }
+}
