677dbb0c6535a9dcaf5b80e51a8ede46fc64bb35
Jovir69 <jovirodriguezgye@gmail.com>
2024-02-14 18:00:26 -0500

    Inicio de la tarea 42

--- arreglos/ejercicios1.js
+++ arreglos/ejercicios1.js
@@ -14,6 +14,27 @@ recorrerArreglo = function(){
     }
 }
 
+generarTabla = function(){
+    let contenidoTabla = ""
+    let cmpTabla = document.getElementById("divTabla")
+    contenidoTabla += "<table><tr><td>1</td></tr>"+"<tr><td>1</td></tr></table>"
+    cmpTabla.innerHTML = contenidoTabla
+}
+
+mostrarNotas = function(){
+    let cmpTabla = document.getElementById("divTabla")
+    let contenidoTabla = "<table><tr><th>NOTA</th></tr>"
+    let miNota
+    for(let i = 0; i < notas.length; i++){
+        miNota = notas[i]
+        contenidoTabla += "<tr><td>"
+        contenidoTabla += miNota
+        contenidoTabla += "</tr></td>"
+    }
+    contenidoTabla += "</table>"
+    cmpTabla.innerHTML = contenidoTabla
+}
+
 probarAgregar = function(){
     let notaRecuperada;
     notaRecuperada = recuperarInt("txtNota");
@@ -22,6 +43,9 @@ probarAgregar = function(){
 
 agregarNota = function(nota){
     notas.push(nota);
+    mostrarTextoEnCaja("txtNota","")
+    document.getElementById("txtNota").focus()
+    mostrarNotas()
 }
 
 calcularPromedio = function(){
