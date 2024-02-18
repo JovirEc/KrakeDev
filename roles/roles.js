97108847352b08f81438834283f19ca162c34aea
Jovir69 <jovirodriguezgye@gmail.com>
2024-02-18 01:28:59 -0500

    Se agregó la tabla + una cedula adicional.

--- roles/roles.js
+++ roles/roles.js
@@ -1,12 +1,14 @@
 let empleados = [
     {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0},
+    {cedula:"0931657005",nombre:"Javier",apellido:"Peterburgos",sueldo:750.0}
 ]
 
 mostrarOpcionEmpleado = function(){
     mostrarComponente("divEmpleado")
     ocultarComponente("divRol")
     ocultarComponente("divResumen")
+    mostrarEmpleados()
 }
 
 mostrarOpcionRol = function(){
@@ -19,4 +21,25 @@ mostrarOpcionResumen = function(){
     mostrarComponente("divResumen")
     ocultarComponente("divEmpleado")
     ocultarComponente("divRol")
+}
+
+mostrarEmpleados = function(){
+    let cmpTabla = document.getElementById("tablaEmpleados")
+    let var1 = empleados.length
+    let contenidoTabla = "<table>"+
+    "<tr><th>CEDULA</th>"+
+    "<th>NOMBRE</th>"+
+    "<th>APELLIDO</th>"+
+    "<th>SUELDO</th></tr>"
+    for(let i = 0; i < var1; i++){
+        let datos = empleados[i]
+        contenidoTabla += "<tr>"+
+        "<td>"+datos.cedula+"</td>"+
+        "<td>"+datos.nombre+"</td>"+
+        "<td>"+datos.apellido+"</td>"+
+        "<td>"+datos.sueldo+"</td>"
+        +"</tr>"
+    }
+    contenidoTabla += "</table>"
+    cmpTabla.innerHTML = contenidoTabla
 }
