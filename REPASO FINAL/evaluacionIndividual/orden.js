08f2b688a2ec6c4a4ff7e442d474682a1ec9f397
Jovir69 <jovirodriguezgye@gmail.com>
2024-02-22 18:01:48 -0500

    Repaso final

---
+++ REPASO FINAL/evaluacionIndividual/orden.js
@@ -0,0 +1,7 @@
+let personas = [
+    {nombre: "Marcos", edad: 18},
+    {nombre: "Roberto", edad: 15},
+    {nombre: "Kate", edad: 25},
+    {nombre: "Diana", edad: 12},
+    {nombre: "Benja", edad: 5},
+]
